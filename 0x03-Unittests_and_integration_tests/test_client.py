43c42fbcacc5f520458e26c4bb07c27dda6f2b89
mesh-254 <meshack1995@gmail.com>
2022-07-19 01:35:33 +0300

    more patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,8 +4,10 @@ a class to test the github_org_client.py file
 """
 from typing import Any
 import unittest
+
+from requests import HTTPError
 from client import GithubOrgClient
-from unittest.mock import MagicMock, PropertyMock, patch
+from unittest.mock import MagicMock, Mock, PropertyMock, patch
 from parameterized import parameterized
 from utils import get_json
 
@@ -85,6 +87,58 @@ class TestGithubOrgClient(unittest.TestCase):
         github_org_client = GithubOrgClient("google")
         self.assertEqual(github_org_client.has_license(
             repo_json, license_key), expected_result)
+
+
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """
+    a class to test integration test fixtures
+    """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        a method to set up class
+        """
+        route_payload = {
+            'https://api.github.com/orgs/google': cls.org_payload,
+            'https://api.github.com/orgs/google/repos': cls.repos_payload,
+        }
+
+        def get_payload(url):
+            """
+            a method to get payload
+            """
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
+            return HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """
+        Tests the public_repos method.
+        """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """
+        Tests the public_repos method with a license.
+        """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """
+        a method to tear down class
+        """
+        cls.get_patcher.stop()
 
 
 if __name__ == '__main__':
