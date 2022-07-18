1160028fd3451ef8532f6b9edd2d1c45afec7949
mesh-254 <meshack1995@gmail.com>
2022-07-19 00:45:40 +0300

    6. More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,6 +6,7 @@ import unittest
 from client import GithubOrgClient
 from unittest.mock import patch
 from parameterized import parameterized
+from utils import get_json
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -26,20 +27,46 @@ class TestGithubOrgClient(unittest.TestCase):
         github_org_client = GithubOrgClient(name)
         github_org_client.org()
         mock_org.assert_called_once()
-    
+
     def test_public_repos_url(self):
         """
         a method to test public_repos_url method
         """
-        with patch ('client.GithubOrgClient.org') as mock_org:
+        with patch('client.GithubOrgClient.org') as mock_org:
             mock_org.return_value = {
-                 "https://api.github.com/orgs/google/repos"
+                "https://api.github.com/orgs/google/repos"
             }
             github_org_client = GithubOrgClient("google")
-            self.assertEqual(github_org_client._public_repos_url, "https://api.github.com/orgs/google/repos")
-            
+            self.assertEqual(github_org_client._public_repos_url,
+                             "https://api.github.com/orgs/google/repos")
 
-            
+    @patch('utils.get_json')
+    def test_public_repos(self, mock_get_json: callable) -> list:
+        """Method to test get_json 
+        & public_repos_url method
+        """
+        mock_get_json.return_value = [
+            {
+                "name": "google",
+                "license": {
+                    "key": "mit"
+                }
+            },
+            {
+                "name": "abc",
+                "license": {
+                    "key": "mit"
+                }
+            }
+        ]
+        mock_get_json.assert_called_once()
+
+        with patch('GithubOrgClient._public_repos_url') as mock_public_repos_url:
+            mock_public_repos_url.return_value = "https://api.github.com/orgs/google/repos"
+            github_org_client = GithubOrgClient("google")
+            self.assertEqual(github_org_client.public_repos(), ["google", "abc"])
+            mock_public_repos_url.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
