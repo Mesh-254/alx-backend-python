6e819ae365c35479d6fe9e9f661209b317288bdc
Mesh-254 <meshack1995@gmail.com>
2024-12-05 02:08:25 +0300

    Implement generator function to stream rows from SQL database
    - Added  function to fetch and yield rows from the  table in the database.

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import os
+import pymysql
+from pymysql.err import Error
+from dotenv import load_dotenv
+
+
+"""generator that streams rows from an SQL database"""
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+def stream_users():
+    """generator function to fetch rows
+    one by one from the user_data table. """
+    try:
+        connection = pymysql.connect(
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_DATABASE')
+        )
+
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute("SELECT * FROM user_data")
+
+                for x in cursor:
+                    yield x
+
+            except Error as e:
+                print(f"Error fetching data: {e}")
+
+            finally:
+                cursor.close()
+
+        return connection
+
+    except Error as e:
+        print(f"Error connecting to ALX_prodev database: {e}")
+        return None
