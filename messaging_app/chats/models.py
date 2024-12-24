335ebb04484b0a8905fc954f4f48e51e65c77b83
Mesh-254 <meshack1995@gmail.com>
2024-12-24 17:50:28 +0300

    Implement robust Django models for User, Conversation, and Message with constraints, relationships, and indexing.

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(models.Model):
+    ROLE_CHOICES = [
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]
+
+    user_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password_hash = models.CharField(max_length=255, null=False)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['email']),
+            models.Index(fields=['user_id']),
+        ]
+
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    participants_id= models.ManyToManyField(User, related_name="conversation")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['conversation_id']),
+        ]
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    sender_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="message")
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="message")
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['message_id']),
+        ]
