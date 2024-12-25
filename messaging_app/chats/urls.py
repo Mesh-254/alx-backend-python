da0c89c1a1f20748abe49fb5b5c50144ea5ab872
Mesh-254 <meshack1995@gmail.com>
2024-12-25 17:33:04 +0300

    Implement API endpoints for listing, creating conversations, and sending messages using viewsets

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
 from chats import views
 
 # Create a router and register our ViewSets with it.
-router = DefaultRouter()
-router.register(r'message', views.MessageViewSet, basename='messages')
-router.register(r'conversation', views.ConversationViewSet, basename='conversations')
+router = routers.DefaultRouter()
+router.register(r'messages', views.MessageViewSet, basename='messages')
+router.register(r'conversations', views.ConversationViewSet, basename='conversations')
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
