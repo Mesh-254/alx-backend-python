e30231c9d0223705f411083a6bac35b228aac0b9
Mesh-254 <meshack1995@gmail.com>
2024-12-27 18:24:25 +0300

    Implement pagination and filtering for messages: Add pagination for 20 messages per page and create MessageFilter class to filter messages by user and time range using django-filters.

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import User, Conversation, Message
 from django.contrib.auth.models import User
 from .serializers import ConversationSerializer, MessageSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsParticipantOfConversation
+from rest_framework.pagination import PageNumberPagination
+from django_filters import rest_framework as filters
 
 class ConversationViewSet(viewsets.ModelViewSet):
     """
@@ -21,6 +23,23 @@ class ConversationViewSet(viewsets.ModelViewSet):
         # Ensure users only see conversations they participate in.
         return Conversation.objects.filter(participants=self.request.user)
 
+# Custom pagination class
+class MessagePagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+# Filter for messages
+class MessageFilter(filters.FilterSet):
+    sender = filters.CharFilter(field_name="sender__username", lookup_expr="icontains")
+    conversation = filters.NumberFilter(field_name="conversation__id")
+    sent_before = filters.DateTimeFilter(field_name="sent_at", lookup_expr="lte")
+    sent_after = filters.DateTimeFilter(field_name="sent_at", lookup_expr="gte")
+
+    class Meta:
+        model = Message
+        fields = ['sender', 'conversation', 'sent_before', 'sent_after']
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     """
@@ -30,6 +49,7 @@ class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = MessageFilter
     ordering_fields = ['sent_at']
     ordering = ['-sent_at']
 
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -137,7 +137,11 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
-    'DEFAULT_FILTER_BACKENDS': (
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    
+    'PAGE_SIZE': 20,  # Default page size for endpoints with pagination
+
+    'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
-    ),
+    ],
 }
